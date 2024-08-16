45f4bd55b9f27eca1cafadb593fbb39d48fb1ea9
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-08-16 20:02:17 +0200

    Endpoint para recuperar los datos de un equipo saca bien los campeonatos

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,10 +1,13 @@
 // Model
 const Team = require("../models/teams")
+const Competition = require("../models/competitions")
+const Result = require("../models/results")
 // Cache
 const { setupCacheAdapter, handleCache } = require("../app/cache")
 const cacheAdapter = setupCacheAdapter(15)
 // Utils
 const getTeamImages = require("../utils/getTeamImages")
+const sortResults = require("../utils/sortResults")
 
 async function getTeams(req, res) {
     console.log("[GET] /api/v/teams");
@@ -63,6 +66,68 @@ async function getTeam(req, res) {
     }
 }
 
+async function getTeamResume(req, res) {
+    console.log("[GET] /api/v/teams/:name/resume");
+
+    const teamName = req.params.name;
+
+    try {
+        const competitions = await Competition.find({ isActive: true, isCancelled: false });
+        const results = await Result.find({ isValid: true /*isFinal: true*/ });
+
+        if (!competitions || !results) {
+            return res.status(404).send({ error: "Missing Data" });
+        }
+
+        let teamResume = [];
+
+        const championships = competitions.filter(competition => competition.isChampionship === true);
+
+        championships.forEach(competition => {
+            let yearResume = teamResume.find(year => year.year === competition.date.getFullYear());
+
+            const resultsForThisCompetition = results.filter(result => result.competition_id == competition._id);
+            const resultsByCategory = resultsForThisCompetition.reduce((acc, result) => {
+                if (!acc[result.category]) acc[result.category] = [];
+                acc[result.category].push(result);
+                return acc;
+            }, {});
+
+            let competitionResults = [];
+
+            Object.entries(resultsByCategory).forEach(([category, categoryResults]) => {
+                const sortedResults = sortResults(categoryResults);
+                const position = sortedResults.findIndex((result) => result.teamShortName === teamName) + 1;
+                let result = sortedResults.filter(res => res.teamShortName === teamName)[0];
+                let a = {
+                    "competition_name": competition.name,
+                    "competition_id": result.competition_id,
+                    "category": result.category,
+                    "position": position
+                };
+                competitionResults.push(a);
+            });
+
+            if (!yearResume) {
+                yearResume = {
+                    "year": competition.date.getFullYear(),
+                    "results": competitionResults,
+                };
+                teamResume.push(yearResume);
+            } else {
+                yearResume.results.push(...competitionResults);
+            }
+        });
+
+        console.log(teamResume);
+        res.status(200).send({ resume: teamResume });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal server error" });
+    }
+}
+
+
 async function postTeams(req, res) {
     console.log("[POST] /api/v/teams");
 
@@ -110,6 +175,7 @@ async function patchTeam(req, res) {
 module.exports = {
     getTeams,
     getTeam,
+    getTeamResume,
     postTeams,
     patchTeam,
 }
--- router/teams.js
+++ router/teams.js
@@ -8,6 +8,7 @@ const api = express.Router();
 api.get("/teams", teamController.getTeams);
 api.post("/teams", imageMiddleware.any(), teamController.postTeams);
 api.get("/teams/:name", teamController.getTeam);
+api.get("/teams/:name/resume", teamController.getTeamResume);
 api.patch("/teams/:id", teamController.patchTeam);
 
 module.exports = api;
